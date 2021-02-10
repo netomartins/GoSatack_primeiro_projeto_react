308d15dd6d194f9b9ea1b53981f57722627c30ac
Neto Martins <netomartins2@hotmail.com>
2021-02-10 12:36:46 -0300

    criando o formulario e o primeiro item de pesquisa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/20827326?s=460&u=e6035e64c4215bf53fbc45baecbc9d2ce2a91ced&v=4"
+            alt="Neto Martins"
+          />
+
+          <div>
+            <strong>Rocketseat/Unform</strong>
+            <p>Easy Highly scalable ReactJs</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
